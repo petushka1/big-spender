1b1c93df66c4a80918652a7e60619b69ac2dd082
petushka1 <nadya@devery.io>
2022-11-05 02:03:12 +0200

    add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_many :groups, dependent: :destroy
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe(User, type: :model) do
-  after { described_class.delete_all }
-
-  describe('with valid arguments') do
-    subject do
-      described_class.new(name: 'TEST_USER', email: 'test@dev.email', password: 'test@123')
-    end
-
-    it { should be_valid }
+  it 'should be valid with arguments' do
+    expect(described_class.new(name: 'Test user', email: 'test@email.com', password: 'password')).to be_valid
   end
 
-  describe('Associations') do
-    it { should have_many(:entities) }
-
-    it { should have_many(:categories) }
+  it('should be invalid') do
+    expect(described_class.new).to_not be_valid
   end
 
-  describe('with invalid arguments') do
-    it('should be invalid') do
-      expect(described_class.new).to_not be_valid
-    end
+  it('should be invalid without password') do
+    user = described_class.new(name: 'user', email: 'test@email.com')
+    expect(user).to_not be_valid
+  end
 
-    it('should fail with less than 3 chars') do
-      user = described_class.new(name: 'HI', email: 'test@dev.email', password: 'test@123')
-      expect(user).to_not be_valid
-    end
+  it('should be invalid with less than 3 chars') do
+    user = described_class.new(name: 'r', email: 'test@some.email', password: 'password')
+    expect(user).to_not be_valid
+  end
 
-    it('should fail without email') do
-      user = described_class.new(name: 'TEST_USER', password: 'test@123')
-      expect(user).to_not be_valid
-    end
-
-    it('should fail without password') do
-      user = described_class.new(name: 'TEST_USER', email: 'test@dev.email')
-      expect(user).to_not be_valid
-    end
+  it('should be invalid without email') do
+    user = described_class.new(name: 'user', password: 'gdjghdjg')
+    expect(user).to_not be_valid
   end
 end
